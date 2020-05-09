b50fc88aaf23efd24310312da8fbd4285481e808
Anh Tu Do <d04nhtu@gmail.com>
2020-05-09 19:43:49 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.d04nhtu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+}
---
+++ src/main/kotlin/search/BinarySearchIterative.kt
@@ -0,0 +1,19 @@
+package search
+
+fun <T : Comparable<T>> binarySearchIterative(array: Array<T>, key: T): Int {
+    var low = 0
+    var high = array.size - 1
+
+    while (low <= high) {
+        val mid = (low + high) / 2
+        val comparedResult = array[mid].compareTo(key)
+
+        when {
+            comparedResult == 0 -> return mid
+            comparedResult > 0 -> high = mid - 1
+            else -> low = mid + 1
+        }
+    }
+
+    return -1
+}
