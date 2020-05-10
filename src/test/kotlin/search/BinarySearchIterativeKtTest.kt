199c03c8d3ae1422aad3de51b484643a0ac2c5d0
Anh Tu Do <d04nhtu@gmail.com>
2020-05-10 07:41:48 +0200

    Implement binary search using recursive

--- src/test/kotlin/search/BinarySearchIterativeKtTest.kt
+++ src/test/kotlin/search/BinarySearchIterativeKtTest.kt
@@ -5,21 +5,21 @@ import org.junit.jupiter.api.Test
 internal class BinarySearchIterativeKtTest {
 
     @Test
-    fun binarySearchIterativeCase1() {
+    fun binarySearchIterativeKtTest1() {
         val array = arrayOf(1, 2, 3, 5, 8)
         val key = 3
         assert(binarySearchIterative(array, key) == 2)
     }
 
     @Test
-    fun binarySearchIterativeCase2() {
+    fun binarySearchIterativeKtTest2() {
         val array = arrayOf(-10, 0, 1, 3, 4, 5, 7, 8, 10)
         val key = 2
         assert(binarySearchIterative(array, key) == -1)
     }
 
     @Test
-    fun binarySearchIterativeCase3() {
+    fun binarySearchIterativeKtTest3() {
         val array = arrayOf("A", "B", "D", "H", "T")
         val key = "H"
         assert(binarySearchIterative(array, key) == 3)
---
+++ src/test/kotlin/search/BinarySearchRecursiveKtTest.kt
@@ -0,0 +1,29 @@
+package search
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BinarySearchRecursiveKtTest {
+
+    @Test
+    fun binarySearchRecursiveKtTest1() {
+        val array = arrayOf(1, 2, 3, 5, 8)
+        val key = 3
+        assert(binarySearchRecursive(array, key) == 2)
+    }
+
+    @Test
+    fun binarySearchRecursiveKtTest2() {
+        val array = arrayOf(-10, 0, 1, 3, 4, 5, 7, 8, 10)
+        val key = 2
+        assert(binarySearchRecursive(array, key) == -1)
+    }
+
+    @Test
+    fun binarySearchRecursiveKtTest3() {
+        val array = arrayOf("A", "B", "D", "H", "T")
+        val key = "H"
+        assert(binarySearchRecursive(array, key) == 3)
+    }
+}
